227929ddee1ad2edf2b98cececd5931dc1c0e11f
Dylan Nguyen <babydylannguyen@gmail.com>
2020-10-13 20:33:04 -0700

    add start func to prompt user

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const inquirer = require("inquirer")
 const mysql = require("mysql");
 require("console.table")
 
+
 const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -10,14 +11,90 @@ const connection = mysql.createConnection({
     database: "employees_db"
   });
 
-connection.connect((err) => {
-    if (err) {
-        console.error(`error connecting: ${ err.stack}`);
-        return;
-    }
-    console.log(`connected as id ${ connection.threadId}`);
-});
+	connection.connect(function (err) {
+		if (err) throw err;
+		start();
+	});
 
-app.listen(PORT, () => {
-	console.log(`Server listening on: http://localhost:${ PORT}`);
-});
+function start() {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "View All Employees",
+        "View All Employees By Department",
+        "View All Employees Manager",
+				"Add Employee",
+				"Remove Employee",
+				"Update Employee Role",
+				"Update Employee Manager",
+				"Exit"
+      ]
+    })
+    .then(function (answer) {
+      switch (answer.action) {
+        case "View All Employees":
+          allEmp();
+          break;
+
+        case "View All Employees By Department":
+          allEmpByDep();
+          break;
+
+        case "View All Employees Manager":
+          allEmpManagers();
+          break;
+
+        case "Add Employee":
+          addEmp();
+					break;
+					
+        case "Remove Employee":
+          remEmp();
+          break;
+
+        case "Update Employee Role":
+          updateEmpRole();
+					break;
+					
+        case "Update Employee Manager":
+          updateEmpManager();
+          break;
+
+        case "Exit":
+          connection.end();
+          break;
+      }
+    });
+}
+
+
+function allEmp() {
+
+}
+
+function allEmpByDep() {
+
+}
+
+function allEmpManagers() {
+
+}
+
+function addEmp() {
+
+}
+
+function remEmp() {
+
+}
+
+function updateEmpRole() {
+
+}
+
+function updateEmpManager() {
+
+}
